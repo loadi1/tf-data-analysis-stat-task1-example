d3b3a3c32e195d4f58529860b75554775056aabf
loadi1 <70744346+loadi1@users.noreply.github.com>
2023-04-03 16:12:05 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,8 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 573772382 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
+    x = x*2/38**2
     return x.mean() # Ваш ответ
